d56480424bd4692cdad52a0c85b606546dcbcfcb
Rodolfo0 <fuzzyrodo@hotmail.com>
2020-04-22 11:57:43 -0500

    Migrations

---
+++ presentacion/Migrations/202004221636477_inicio.cs
@@ -0,0 +1,16 @@
+﻿namespace presentacion.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class inicio : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
